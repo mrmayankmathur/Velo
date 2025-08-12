4b02c137a5b29d871a270277e25b348ea92219e4
mrmayankmathur <mrmayankmathur@gmail.com>
2025-08-12 21:38:02 +0530

    AI Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const Page = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,9 +20,10 @@ const Page = () => {
   );
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "Mayank" })}
+        onClick={() => invoke.mutate({ value: value })}
       >
         Invoke background job
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,27 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    // Creating a new agent with a system prompt (we can add optional tools, too)
+    const summarizer = createAgent({
+      name: "summarizer",
+      system:
+        "You are an expert Next.JS Developer. You write readable, maintainable code. You write simple Next.JS and React snippets.",
+      model: openai({
+        model: "gpt-4o",
+        apiKey: process.env.GITHUB_TOKEN,
+        baseUrl: "https://models.github.ai/inference",
+      }),
+    });
+
+    const { output } = await summarizer.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   }
 );
